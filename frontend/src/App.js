bc5c3248ed9add6ab3b68745abe6e6ffbaf91b0a
suraj tetarwal <lord.skt210503@gmail.com>
2024-11-02 11:58:21 +0530

    Update existing files and remove unused components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-import TestLogin from './components/TestLogin'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
-import './App.css';
+import SignUp from './components/SignUp'
+import SignIn from './components/SignIn'
 
-function App() {
+import './App.css'
+
+const App = () => {
   return (
-    <TestLogin />
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
